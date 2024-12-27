dc5082a566a899843cb1dd5a877e0eb6d5f0acb4
Zondi-313 <bereghaj@gmail.com>
2024-12-27 19:10:49 +0100

    first comment

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+            System.out.println("Hello User please Enter your name:");
+            String name=sc.nextLine();
+            System.out.println("Enter yoiur lucky number");
+            int lucky=sc.nextInt();
+            System.out.println("Hello "+name +", Your lucky number " +lucky +" shows you are really hardworking. Keep it up!");
+    }
+}
